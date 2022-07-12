c8e26d2988b312f108aeeffe0e1f475aeed09ecb
OlivieroPinto <pintooliviero@gmail.com>
2022-07-12 16:29:20 +0200

    aggiunto saluti randomizzati

--- src/main/java/jana60/hellospring/controller/MainController.java
+++ src/main/java/jana60/hellospring/controller/MainController.java
@@ -2,6 +2,7 @@ package jana60.hellospring.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Random rand = new Random();
+		int n = rand.nextInt(5);
+		String[] salutini = { "Ciao carissimi", "Hello dear", "Che la forza sia con te", "Saluto generico", "Ciá",
+				"Ja mo basta ciao." };
+		model.addAttribute("saluto", salutini[n]);
 		return "home";
 	}
 
